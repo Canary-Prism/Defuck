2a3f360da332d8b7e589ff18f447f141bf03b43e
Canary-Prism <canaryprsn@gmail.com>
2024-01-12 11:56:44 +0800

    nvm

--- src/main/java/canaryprism/brainfuck/defuck/swift/FlowTranspilerSwift.java
+++ src/main/java/canaryprism/brainfuck/defuck/swift/FlowTranspilerSwift.java
@@ -1,4 +1,4 @@
-package canaryprism.brainfuck.defuck.java;
+package canaryprism.brainfuck.defuck.swift;
 
 import java.io.IOException;
 import java.io.OutputStream;
@@ -7,26 +7,25 @@ import java.util.ArrayList;
 import canaryprism.brainfuck.defuck.Decompiler;
 import canaryprism.brainfuck.optimising.FlowInterpreter;
 import canaryprism.brainfuck.optimising.FlowInterpreter.OptimisedCollapsedInstruction;
-import canaryprism.writers.SourcecodeWriterJava;
+import canaryprism.writers.SourcecodeWriterSwift;
 
-//slow as fuck and very not worth it :3
-public class BasicTranspilerJava extends Decompiler {
+//somehow even slower 
+public class FlowTranspilerSwift extends Decompiler {
 
     private final ArrayList<OptimisedCollapsedInstruction> code;
 
-    public BasicTranspilerJava(String code) {
+    public FlowTranspilerSwift(String code) {
         this.code = new FlowInterpreter(code).getOptimisedCode();
     }
 
     @Override
     @SuppressWarnings("resource")
     public void decompile(OutputStream out) throws IOException {
-        try (var wr = new SourcecodeWriterJava(out)) {
+        try (var wr = new SourcecodeWriterSwift(out, true)) {
             wr
-                .startClass("Main")
-                    .psvm()
-                        .code("var memory = new int[60_000];")
-                        .code("var pointer = 30_000;");
+                        .writeImport("Darwin")
+                        .code("var memory = [Int](repeating: 0, count: 60_000)")
+                        .code("var pointer = 30_000");
 
             for (var e : code) {
                 switch (e.instruction()) {
@@ -38,23 +37,19 @@ public class BasicTranspilerJava extends Decompiler {
                             wr.endWhile();
                         }
                     }
-                    case move -> wr.code(STR."pointer += \{e.a()};");
-                    case in -> wr.code("memory[pointer] = System.in.read() & 255;");
-                    case out -> wr.code("System.out.write((char)memory[pointer]);");
+                    case move -> wr.code(STR."pointer += \{e.a()}");
+                    case in -> wr.code("memory[pointer] = Int(fgetc(stdin))");
+                    case out -> wr.code("fputc(Int32(memory[pointer]), stdout)");
                     case findzero -> wr
                         .startWhile("memory[pointer] != 0")
-                            .code(STR."pointer += \{e.a()};")
+                            .code(STR."pointer += \{e.a()}")
                         .endWhile();
-                    case set -> wr.code(STR."memory[pointer] = \{e.a()};");
-                    case transfer -> wr.code(STR."memory[pointer + \{e.a()}] = (memory[pointer + \{e.a()}] + memory[pointer] * \{e.b()}) & 255;");
-                    case end -> wr.code("return;");
+                    case set -> wr.code(STR."memory[pointer] = \{e.a()}");
+                    case transfer -> wr.code(STR."memory[pointer + \{e.a()}] = (memory[pointer + \{e.a()}] + memory[pointer] * \{e.b()}) & 255");
+                    case end -> wr.code("// end of code");
                     case none -> wr.code("// WARNING: none written to sourcecode here");
                 }
             }
-
-            wr
-                    .endMethod()
-                .endClass();
         } 
     }
     
